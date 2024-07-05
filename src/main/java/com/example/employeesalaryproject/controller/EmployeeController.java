27bf2acd4df0936dd713ba2292ac75e091dda723
Lohith Kumar <lohithkumar2703@gmail.com>
2024-07-04 23:00:19 -0500

    GET Mapping.

--- src/main/java/com/example/employeesalaryproject/controller/EmployeeController.java
+++ src/main/java/com/example/employeesalaryproject/controller/EmployeeController.java
@@ -5,14 +5,16 @@ import com.example.employeesalaryproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/employee/{id}")
+    @GetMapping("/{id}")
     public EmployeeDetails getEmployeeById(@PathVariable("id") Long id) {
         return employeeService.getEmployeeById(id);
     }
