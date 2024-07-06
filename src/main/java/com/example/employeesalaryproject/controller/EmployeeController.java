7e868e947d9f143ec231a6856c96e770723775f4
Lohith Kumar <lohithkumar2703@gmail.com>
2024-07-05 22:41:52 -0500

    POST and DELETE Mapping.

--- src/main/java/com/example/employeesalaryproject/controller/EmployeeController.java
+++ src/main/java/com/example/employeesalaryproject/controller/EmployeeController.java
@@ -3,19 +3,36 @@ package com.example.employeesalaryproject.controller;
 import com.example.employeesalaryproject.pojo.EmployeeDetails;
 import com.example.employeesalaryproject.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/employee")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/{id}")
+    @GetMapping("/employee/{id}")
     public EmployeeDetails getEmployeeById(@PathVariable("id") Long id) {
         return employeeService.getEmployeeById(id);
+    }
+
+    @GetMapping("/employee")
+    public double getEmployeeByName(@RequestParam String name) {
+        EmployeeDetails employeeByName = employeeService.getEmployeeByName(name);
+        if (employeeByName == null) {
+            System.out.println("Employee Null.");
+        }
+        double netSalary = employeeService.calculateNetSalary(employeeByName);
+        return netSalary;
+    }
+
+    @DeleteMapping("/employee")
+    public String deleteEmployeeById(@RequestParam Long id) {
+        System.out.println("Employee Record to be deleted: " + id);
+        return "Record Deleted";
+    }
+
+    @PostMapping("/employee")
+    public EmployeeDetails createEmployee(@RequestBody EmployeeDetails employeeDetails) {
+        return employeeService.createEmployee(employeeDetails);
     }
 }
--- src/main/java/com/example/employeesalaryproject/service/EmployeeService.java
+++ src/main/java/com/example/employeesalaryproject/service/EmployeeService.java
@@ -21,4 +21,40 @@ public class EmployeeService {
         BeanUtils.copyProperties(employeeById, employeeDetails);
         return employeeDetails;
     }
+
+    public EmployeeDetails getEmployeeByName(String name) {
+        Employee employeeByName = employeeRepository.findByName(name).orElse(null);
+        if (employeeByName == null) {
+            throw new RuntimeException("No Employee Record Found with " + name);
+        }
+        EmployeeDetails employeeDetailsWithName = new EmployeeDetails();
+        BeanUtils.copyProperties(employeeByName, employeeDetailsWithName);
+        return employeeDetailsWithName;
+    }
+
+    public EmployeeDetails createEmployee(EmployeeDetails employeeDetails) {
+        Employee newEmployee = new Employee();
+        newEmployee.setName(employeeDetails.getName());
+        newEmployee.setBasic(employeeDetails.getBasic());
+        newEmployee.setHra(employeeDetails.getHra());
+        newEmployee.setDa(employeeDetails.getDa());
+        newEmployee.setDeduction(employeeDetails.getDeduction());
+
+        Employee savedEmployee = employeeRepository.save(newEmployee);
+        BeanUtils.copyProperties(savedEmployee, employeeDetails);
+        return employeeDetails;
+    }
+
+    public double calculateGrossSalary(EmployeeDetails employeeDetails) {
+        return employeeDetails.getBasic() + employeeDetails.getHra() + employeeDetails.getDa();
+    }
+
+    public double calculateNetSalary(EmployeeDetails employeeDetails) {
+        double grossSalary = calculateGrossSalary(employeeDetails);
+        double totalDeductions = employeeDetails.getDeduction();
+        double netSalaryBeforeTax = grossSalary - totalDeductions;
+        double tax = netSalaryBeforeTax * 0.15;
+        double netSalary = netSalaryBeforeTax - tax;
+        return netSalary;
+    }
 }
